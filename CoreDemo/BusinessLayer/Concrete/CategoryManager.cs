0b7f9690658c55e09adcd5fc3162b1a8e40466b9
Emineckmkc <emine.cakmakci36@erzurum.edu.tr>
2022-04-10 17:18:18 +0300

    Entity ve Data Acces operasyonları yazıldı.
    Entity ve Data Acces operasyonları yazıldı.

---
+++ CoreDemo/BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Abstract;
+using DataAccesslayer.Abstract;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+        public void CategoryAdd(Category p)
+        {
+            _categoryDal.Insert(p);
+        }
+
+        public void ContegoryDelete(Category p)
+        {
+            _categoryDal.Delete(p);
+        }
+
+        public void ContegoryUpdate(Category p)
+        {
+            _categoryDal.Update(p);
+        }
+    }
+}
---
+++ CoreDemo/DataAccesslayer/Migrations/20220410125616_mig4.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccesslayer.Migrations
+{
+    public partial class mig4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Blogs_BlogID1",
+                table: "Blogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_BlogID1",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "BlogID1",
+                table: "Blogs");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BlogID1",
+                table: "Blogs",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_BlogID1",
+                table: "Blogs",
+                column: "BlogID1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Blogs_BlogID1",
+                table: "Blogs",
+                column: "BlogID1",
+                principalTable: "Blogs",
+                principalColumn: "BlogID");
+        }
+    }
+}
